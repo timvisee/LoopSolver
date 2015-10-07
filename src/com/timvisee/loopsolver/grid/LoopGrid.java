e9376c150f3095f0eb07734d0a7e1a16a48d52aa
Tim Visée <timvisee@gmail.com>
2015-10-07 18:00:46 +0200

    Added tile grid tiles list, coordinate system, and method to get and set tiles

--- src/com/timvisee/loopsolver/grid/LoopGrid.java
+++ src/com/timvisee/loopsolver/grid/LoopGrid.java
@@ -1,11 +1,19 @@
 package com.timvisee.loopsolver.grid;
 
+import com.timvisee.loopsolver.tile.LoopTile;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoopGrid {
 
     /** Grid width. */
     private int width;
     /** Grid heigth. */
     private int height;
+
+    /** List of tiles in this grid. */
+    private List<LoopTile> tiles = new ArrayList<LoopTile>();
 
     /**
      * Constructor.
@@ -35,5 +43,82 @@ public class LoopGrid {
      */
     public int getHeight() {
         return this.height;
+    }
+
+    /**
+     * Get the total number of tiles in this grid.
+     *
+     * @return Total number of tiles in this grid.
+     */
+    public int getTotal() {
+        return this.getWidth() * this.getHeight();
+    }
+
+    /**
+     * Get the full list of tiles.
+     *
+     * @return Full list of tiles.
+     */
+    public List<LoopTile> getTiles() {
+        return this.tiles;
+    }
+
+    /**
+     * Get the tile list position for a specific location.
+     *
+     * @param x The x coordinate (horizontal from the left), zero-indexed.
+     * @param y The y coordinate (vertical from the top), zero-indexed.
+     *
+     * @return The tile list position for this tile.
+     */
+    public int getTilePosition(int x, int y) {
+        // TODO: Make sure the position isn't out of bound!
+        return x + (y * this.getWidth());
+    }
+
+
+    /**
+     * Get the tile at a specific location.
+     *
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     *
+     * @return The tile.
+     */
+    public LoopTile getTile(int x, int y) {
+        return getTile(getTilePosition(x, y));
+    }
+
+    /**
+     * Get the tile at a specific position.
+     *
+     * @param position The position in the tile list.
+     *
+     * @return The tile.
+     */
+    public LoopTile getTile(int position) {
+        return this.tiles.get(position);
+    }
+
+    /**
+     * Set the tile at a specific location.
+     *
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     *
+     * @param tile The tile.
+     */
+    public void setTile(int x, int y, LoopTile tile) {
+        setTile(getTilePosition(x, y), tile);
+    }
+
+    /**
+     * Set the tile at a specific position.
+     *
+     * @param position The position.
+     * @param tile The tile.
+     */
+    public void setTile(int position, LoopTile tile) {
+        this.tiles.set(position, tile);
     }
 }
