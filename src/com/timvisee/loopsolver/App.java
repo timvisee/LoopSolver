5039fc5a6134aced368321d0754eb7d55e8f4c58
Tim Visée <timvisee@gmail.com>
2015-10-07 18:22:50 +0200

    Created frame to render the grid

--- src/com/timvisee/loopsolver/App.java
+++ src/com/timvisee/loopsolver/App.java
@@ -1,5 +1,11 @@
 package com.timvisee.loopsolver;
 
+import com.timvisee.loopsolver.frame.GridFrame;
+import com.timvisee.loopsolver.grid.LoopGrid;
+import com.timvisee.loopsolver.tile.LoopTile;
+
+import java.util.Random;
+
 public class App {
 
     /**
@@ -20,7 +26,22 @@ public class App {
         // Show an initialization message
         System.out.println("Initializing " + LoopSolver.APP_NAME + "...");
 
+        LoopGrid grid = new LoopGrid(10, 10);
+        GridFrame frame = new GridFrame(grid);
+
         // Show an initialized message
         System.out.println(LoopSolver.APP_NAME + " initialized! Cave Johnson here.");
+    }
+
+    // TODO: Remove this method!
+
+    /**
+     * Generate a random tile.
+     *
+     * @return Random tile.
+     */
+    public static LoopTile getRandomTile() {
+        Random rand = new Random();
+        return new LoopTile(rand.nextBoolean(), rand.nextBoolean(), rand.nextBoolean(), rand.nextBoolean());
     }
 }
---
+++ src/com/timvisee/loopsolver/frame/GridFrame.java
@@ -0,0 +1,58 @@
+package com.timvisee.loopsolver.frame;
+
+import com.timvisee.loopsolver.LoopSolver;
+import com.timvisee.loopsolver.grid.LoopGrid;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GridFrame extends JFrame {
+
+    /** Frame title. */
+    private static final String FORM_TITLE = LoopSolver.APP_NAME;
+
+    private LoopGrid grid;
+
+    /**
+     * Constructor.
+     */
+    public GridFrame(LoopGrid grid) {
+        // Construct the form
+        super(FORM_TITLE);
+
+        // Set the grid
+        this.grid = grid;
+
+        // Create the form UI
+        createUIComponents();
+
+        // Close application when closing form
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Show the form
+        this.setVisible(true);
+    }
+
+    /**
+     * Create all UI components for the frame.
+     */
+    private void createUIComponents() {
+        // Set the frame layout
+        this.setLayout(new GridLayout(this.grid.getHeight(), this.grid.getWidth()));
+
+        // Set the frame size
+        this.setSize(500, 500);
+
+        // Make the frame non-resizable
+        this.setResizable(true);
+
+        // Set the window location to the system's default
+        this.setLocationByPlatform(true);
+        this.setLocationRelativeTo(null);
+
+        // TODO: Do this properly!
+
+        for(int i = 0; i < this.grid.getTotal(); i++)
+            this.add(new JButton(String.valueOf(this.grid.getTile(i).getConnectibleSides())));
+    }
+}
