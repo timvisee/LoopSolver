0406e7acf182cf2dd164100b6d838e036986db31
Tim Visée <timvisee@gmail.com>
2015-10-07 17:17:40 +0200

    Created LoopTileSide enum

---
+++ src/com/timvisee/loopsolver/tile/LoopTileSide.java
@@ -0,0 +1,31 @@
+package com.timvisee.loopsolver.tile;
+
+public enum LoopTileSide {
+
+    /** Enum sides. */
+    TOP(0),
+    RIGHT(1),
+    BOTTOM(2),
+    LEFT(3);
+
+    /** Side number. */
+    private final int side;
+
+    /**
+     * Constructor.
+     *
+     * @param side Side number.
+     */
+    LoopTileSide(int side) {
+        this.side = side;
+    }
+
+    /**
+     * Get the side number.
+     *
+     * @return Side number.
+     */
+    private int side() {
+        return side;
+    }
+}
